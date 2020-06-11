0b48bc0e16df2a30f09585afd212fe9f7b32a64b
reginadiana <dianaregina22@outlook.com.br>
2020-06-11 16:40:51 -0300

    Escrita de teste de model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+	it('create a new task with sucess') do
+		title = FFaker::Name.first_name
+		done = %i[true false].sample
 
+		task = Task.new(title: title, done: done)
+		expect(task).to be_valid
+	end
+
+	it('task cannot be blank') do
+		done = %i[true false].sample
+
+		task = Task.new(title: " ", done: done)
+		expect(task).to_not be_valid
+	end
 end
